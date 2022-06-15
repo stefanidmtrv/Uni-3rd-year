7f5ae827b4c82c55b6d0548150a268620d42fe48
Stefani Dimitrova <74249819+stefanidmtrv@users.noreply.github.com>
2022-06-15 15:08:16 +0300

    Add files via upload

---
+++ Advanced Object Oriented Programming/Labs/Lab1/task2/cat.h
@@ -0,0 +1,51 @@
+#ifndef CAT_H
+#define CAT_H
+#pragma once
+
+class Cat
+{
+
+  
+// Private members.
+private:
+  std::string name;
+  unsigned int lives;
+  friend std::ostream& operator<<(std::ostream& os, const Cat& cat);
+  friend bool operator==(const Cat& lhs, const Cat& rhs);
+  
+public:
+  // Constructors.
+  Cat();
+  Cat(Cat &&other) noexcept; // move constructor.
+  Cat(std::string name);
+  Cat(std::string name, unsigned int lives);
+
+  Cat &operator=(Cat &&other) noexcept; 
+
+  // Destructor.
+  ~Cat();
+
+  // Getters.
+  std::string &get_name();
+  unsigned int &get_lives();
+
+  // Setters.
+  void set_name(std::string &name);
+  void set_lives(unsigned int &lives);
+
+  // Other methods.
+  std::string to_string() const;
+
+};
+
+namespace std {
+    template <>
+    struct hash <Cat> {
+      size_t operator()(const Cat &obj) const {
+        
+        return std::hash<std::string>()(obj.to_string());
+      }
+    };
+  }
+
+#endif
